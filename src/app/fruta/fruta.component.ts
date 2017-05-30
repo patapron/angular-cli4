bd06b7edee6cb07586c46692a8cfbf53d48b9ca9
Miguel Ángel <pronick@gmail.com>
2017-05-30 23:27:22 +0200

    Sección 4, clase 16 Tipos de datos

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -1,11 +1,17 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'fruta',
   templateUrl: './fruta.component.html'
 })
 
-export class FrutaComponent{
+export class FrutaComponent {
   public nombre_componente = 'Componente de fruta';
   public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
+
+  public cadena: string = "cadena"
+  public numero: number = 23
+  public boleano: boolean = true
+  public variableArray: Array<string> = ["cadena1", "cadena2"]
+  public tipoAny: Array<any> = ["cadena1", "12"]
 }
