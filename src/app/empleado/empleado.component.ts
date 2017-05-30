d5b8780846c5cfdb4e54dff0b27fb9a8493cf1f6
Miguel Ángel <pronick@gmail.com>
2017-05-31 00:20:03 +0200

    sección4, clase19 Crear clases, modelos de datos y objetos

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,12 +1,31 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Empleado } from './empleado';
+
 
 @Component({
     selector: 'empleado-tag',
     templateUrl: './empleado.component.html'
 })
 
-export class EmpleadoComponent{
+export class EmpleadoComponent {
 
-    public titulo = 'Titulo del componente empleado';
+    public titulo = 'Componente empleado';
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+    constructor() {
+        this.empleado = new Empleado('Miguel', 39, 'programador', true);
+        this.trabajadores = [
+            new Empleado('pablo', 23, 'junior', true),
+            new Empleado('nacho', 54, 'becario', true),
+            new Empleado('jose', 21, 'leader', false)
+        ];
+
+    }
+
+
+    ngOnInit() {
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
 
 }
