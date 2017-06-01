737493bf781658797975ac6d9e7ab93cb7e2539b
Miguel Ángel <pronick@gmail.com>
2017-06-01 22:24:38 +0200

    Seccion 5, clase 22 directiva ngIf

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -12,20 +12,26 @@ export class EmpleadoComponent {
     public titulo = 'Componente empleado';
     public empleado: Empleado;
     public trabajadores: Array<Empleado>;
+    public trabajador_externo:boolean;
+
     constructor() {
-        this.empleado = new Empleado('Miguel', 39, 'programador', true);
+        this.empleado = new Empleado('Miguel', 39, 'programador', false);
         this.trabajadores = [
             new Empleado('pablo', 23, 'junior', true),
             new Empleado('nacho', 54, 'becario', true),
             new Empleado('jose', 21, 'leader', false)
         ];
-
+        this.trabajador_externo = false;
     }
 
 
     ngOnInit() {
         console.log(this.empleado);
         console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor;
     }
 
 }
