8c7ffeb72371d470775365cea8e4d9010fb3a931
Miguel Ángel Navas Giraldo <pronick@gmail.com>
2017-06-15 14:26:40 +0200

    add material and flex succesfully

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdMenuModule, MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './fruta/fruta.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +20,17 @@ import { EmpleadoComponent } from './empleado/empleado.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdMenuModule,
+    FlexLayoutModule
+  ],exports:[
+        BrowserAnimationsModule,
+        NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
